af1c3c38db146d897941917d56277d743aedf107
MrBita <tuncozgur423@gmail.com>
2024-05-15 19:09:17 +0300

    karakter hareketlendi ve birkaç mesh düzenlendi

--- OynaOgren/Assets/script_O/PlayerController.cs
+++ OynaOgren/Assets/script_O/PlayerController.cs
@@ -1,56 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Jobs;
 
 public class PlayerController : MonoBehaviour
 {
     public CharacterController characterController;
-    public float speed = 4f;
+    public float speed = 3f;
     public float gravity = -15f;
-    public float velocity = 1f;
-    public Rigidbody rgb3d;
 
     private Vector3 gravityVector;
-    //GroundCheeck
+
+
+    //GroundCheck
     public Transform groundCheckPoint;
     public float groundCheckRadius = 0.35f;
     public LayerMask groundLayer;
 
-    public bool isGrounded;
-
-    public float jumpSpeed = 0.05f;
-
+    public bool isGrounded = false;
+    public float jumpSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveVector = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
-       
-        characterController.Move(moveVector * speed * Time.deltaTime);
+       Vector3 moveVector = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+
+       characterController.Move(moveVector * speed * Time.deltaTime);
 
         gravityVector.y += gravity * Time.deltaTime;
 
         characterController.Move(gravityVector * Time.deltaTime);
 
-        isGrounded = Physics.CheckSphere(groundCheckPoint.position, groundCheckRadius, groundLayer);
-        if (isGrounded && gravityVector.y < 0)
+        isGrounded = Physics.CheckSphere(groundCheckPoint.position, groundCheckRadius, groundLayer); 
+
+        if (isGrounded = true && gravityVector.y < 0)
         {
             gravityVector.y = -3f;
         }
 
-        if(Input.GetButtonDown("Jump") && isGrounded) 
+        if(Input.GetButtonDown("Jump") && isGrounded)
         {
-            gravityVector.y = jumpSpeed;
+
+            gravityVector.y = Mathf.Sqrt(jumpSpeed * -0.3f * gravity);
+            //gravityVector.y = jumpSpeed;
         }
-
-
     }
 }

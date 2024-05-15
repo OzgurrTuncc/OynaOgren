33dfd8b3e5bdc326501ffedde0187f4cb0f693ad
damlanurunal <damlanuur.unall@gmail.com>
2024-05-16 02:35:49 +0300

    hareketler eklendi

---
+++ OynaOgren/Assets/script_D/Door.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "door")
+        {
+            Animator animator = other.GetComponentInChildren<Animator>();
+            animator.SetTrigger("open_close");
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "door")
+        {
+            Animator animator = other.GetComponentInChildren<Animator>();
+            animator.SetTrigger("open_close");
+
+        }
+
+    }
+
+}
